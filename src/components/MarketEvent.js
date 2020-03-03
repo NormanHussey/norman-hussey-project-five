3c09945ab983f22c8470c6b8318c72359fa29868
Norman Hussey <norm.hussey@gmail.com>
2020-03-03 11:00:20 -0500

    added homepage data to package.json to prevent deployment problems. successfully deployed current build. minor text fixes

--- src/components/MarketEvent.js
+++ src/components/MarketEvent.js
@@ -12,11 +12,11 @@ class MarketEvent extends Component {
         let eventText = ``;
         if (this.props.eventInfo.type === 'overabundance') {
             eventText = `
-                The market in ${this.props.location.name} has been flooded with cheap ${this.props.eventInfo.item.type}. Prices have bottomed out!
+                The market in ${this.props.location.name} has been flooded with ${this.props.eventInfo.item.type}. Prices have bottomed out!
             `;
         } else {
             eventText = `
-                ${this.props.eventInfo.item.type} has become scarce in ${this.props.location.name}. The market in is paying top dollar for it!
+                ${this.props.eventInfo.item.type} has become scarce in ${this.props.location.name}. The market here is paying top dollar for it!
             `;
         }
         this.setState({
